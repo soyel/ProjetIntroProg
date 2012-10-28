3ae252a5b7e615ea579d56209f0be55d8e2efd50
Roman <romankos2k@gmail.com>
2012-10-28 13:22:30 +0100

    MENU SDL

--- Etape4/ProjetIntroProg - Etape4/main.c
+++ Etape4/ProjetIntroProg - Etape4/main.c
@@ -5,7 +5,7 @@
 #include <SDL_ttf.h>
 #include <fmod.h>
 
-void pause();
+void afficher_titre_menu();
 
 int main(int argc, char *argv[])
 {
@@ -15,18 +15,15 @@ int main(int argc, char *argv[])
     SDL_Surface *imageDeFond2 = NULL;
     SDL_Surface *boutonJouer = NULL;
     SDL_Surface *boutonQuitter = NULL;
-    SDL_Surface *titre = NULL;
-    SDL_Surface *texte = NULL;
+    SDL_Surface *fleche = NULL;
 
     SDL_Rect positionFond;
     SDL_Rect positionFond2;
 
-    SDL_Rect positionTexte;
-
     SDL_Rect positionBoutonJouer;
     SDL_Rect positionBoutonQuitter;
 
-    SDL_Rect positionTitre;
+    SDL_Rect positionFleche;
 
     positionBoutonJouer.x = 260;
     positionBoutonJouer.y = 250;
@@ -67,7 +64,7 @@ int main(int argc, char *argv[])
     SDL_Color couleurBlanche = {255, 255, 255} ;
 
     SDL_WM_SetIcon(SDL_LoadBMP("laby.bmp"), NULL); //icone
-    ecran = SDL_SetVideoMode(800, 400, 32, SDL_HWSURFACE); // Ouverture de la fenêtre
+    ecran = SDL_SetVideoMode(800, 400, 32, SDL_HWSURFACE | SDL_DOUBLEBUF); // Ouverture de la fenêtre
     SDL_WM_SetCaption("LA'WARA", NULL); //titre
 
     imageDeFond = SDL_LoadBMP("bg.bmp");
@@ -109,50 +106,98 @@ int main(int argc, char *argv[])
                     { //si on click gauche sur 'jouer'
                         SDL_FreeSurface(boutonJouer);
                         SDL_FreeSurface(boutonQuitter);
+                        SDL_FreeSurface(imageDeFond);
+
                         FMOD_System_PlaySound(system, FMOD_CHANNEL_FREE, click, 0, NULL);
                         imageDeFond2 = IMG_Load("bg2.png");
                         SDL_BlitSurface(imageDeFond2, NULL, ecran, &positionFond2);
 
-                        titre = IMG_Load("titre.png");
-                        positionTitre.x = ecran->w / 2 - titre->w / 2;
-                        positionTitre.y = 10;
+                        afficher_titre_menu(ecran);
 
-                        texte = TTF_RenderText_Blended(police, "- Créer un labyrinthe        ", couleurBlanche);
-                        positionTexte.x = ecran->w / 2 - texte->w / 2;
-                        positionTexte.y = 100;
+                        fleche = IMG_Load("fleche.png");
+                        positionFleche.x = 50;
+                        positionFleche.y = 93;
 
-                        SDL_BlitSurface(titre, NULL, ecran, &positionTitre);
-
-                        SDL_BlitSurface(texte, NULL, ecran, &positionTexte);
+                        SDL_BlitSurface(fleche, NULL, ecran, &positionFleche);
                         SDL_Flip(ecran);
 
-                        texte = TTF_RenderText_Blended(police, "- Charger un labyrinthe      ", couleurBlanche);
-                        positionTexte.x = ecran->w / 2 - texte->w / 2;
-                        positionTexte.y = 150;
+                        while(continuer)
+                        {
+                            SDL_WaitEvent(&event); /* On attend un événement qu'on récupère dans event */
+                            switch(event.type) /* On teste le type d'événement */
+                            {
+                                case SDL_QUIT: /* Si c'est un événement QUITTER */
+                                    continuer = 0; /* On met le booléen à 0, donc la boucle va s'arrêter */
+                                    break;
 
-                        SDL_BlitSurface(texte, NULL, ecran, &positionTexte);
-                        SDL_Flip(ecran);
+                                case SDL_KEYDOWN: /* Si appui sur une touche */
+                                    if (event.key.keysym.sym == SDLK_ESCAPE)
+                                    {
+                                            continuer = 0;
+                                            break;
+                                    }
+                                case SDL_MOUSEMOTION:
+                                    if(event.motion.x >= 150 && event.motion.x <= 694 && event.motion.y >= 100 && event.motion.y <= 140)
+                                    {
+                                        SDL_BlitSurface(imageDeFond2, NULL, ecran, &positionFond2);
 
-                        texte = TTF_RenderText_Blended(police, "- Jouer une partie par défaut", couleurBlanche);
-                        positionTexte.x = ecran->w / 2 - texte->w / 2;
-                        positionTexte.y = 200;
+                                        afficher_titre_menu(ecran);
+                                        positionFleche.y = 100;
+                                        SDL_BlitSurface(fleche, NULL, ecran, &positionFleche);
 
-                        SDL_BlitSurface(texte, NULL, ecran, &positionTexte);
-                        SDL_Flip(ecran);
+                                        SDL_Flip(ecran);
+                                    }
+                                    if(event.motion.x >= 150 && event.motion.x <= 694 && event.motion.y >= 140 && event.motion.y <= 180)
+                                    {
+                                        SDL_BlitSurface(imageDeFond2, NULL, ecran, &positionFond2);
 
-                        texte = TTF_RenderText_Blended(police, "- Trouver la solution du laby", couleurBlanche);
-                        positionTexte.x = ecran->w / 2 - texte->w / 2;
-                        positionTexte.y = 250;
+                                        afficher_titre_menu(ecran);
+                                        positionFleche.y = 140;
+                                        SDL_BlitSurface(fleche, NULL, ecran, &positionFleche);
 
-                        SDL_BlitSurface(texte, NULL, ecran, &positionTexte);
-                        SDL_Flip(ecran);
+                                        SDL_Flip(ecran);
+                                    }
+                                    if(event.motion.x >= 150 && event.motion.x <= 694 && event.motion.y >= 180 && event.motion.y <= 220)
+                                    {
+                                        SDL_BlitSurface(imageDeFond2, NULL, ecran, &positionFond2);
 
-                        texte = TTF_RenderText_Blended(police, "- Quitter                    ", couleurBlanche);
-                        positionTexte.x = ecran->w / 2 - texte->w / 2;
-                        positionTexte.y = 300;
+                                        afficher_titre_menu(ecran);
+                                        positionFleche.y = 180;
+                                        SDL_BlitSurface(fleche, NULL, ecran, &positionFleche);
 
-                        SDL_BlitSurface(texte, NULL, ecran, &positionTexte);
-                        SDL_Flip(ecran);
+                                        SDL_Flip(ecran);
+                                    }
+                                    if(event.motion.x >= 150 && event.motion.x <= 694 && event.motion.y >= 220 && event.motion.y <= 260)
+                                    {
+                                        SDL_BlitSurface(imageDeFond2, NULL, ecran, &positionFond2);
+
+                                        afficher_titre_menu(ecran);
+                                        positionFleche.y = 220;
+                                        SDL_BlitSurface(fleche, NULL, ecran, &positionFleche);
+
+                                        SDL_Flip(ecran);
+                                    }
+                                    if(event.motion.x >= 150 && event.motion.x <= 694 && event.motion.y >= 260 && event.motion.y <= 300)
+                                    {
+                                        SDL_BlitSurface(imageDeFond2, NULL, ecran, &positionFond2);
+
+                                        afficher_titre_menu(ecran);
+                                        positionFleche.y = 260;
+                                        SDL_BlitSurface(fleche, NULL, ecran, &positionFleche);
+
+                                        SDL_Flip(ecran);
+                                    }
+                                case SDL_MOUSEBUTTONUP:
+                                    if (event.button.button == SDL_BUTTON_LEFT)
+                                    {
+                                        if(event.button.x > 150 && event.button.x < 694 && event.button.y > 260 && event.button.y < 300 )
+                                        { //si on click gauche sur 'quitter'
+                                            FMOD_System_PlaySound(system, FMOD_CHANNEL_FREE, click, 0, NULL);
+                                            continuer = 0;
+                                        }
+                                    }
+                            }
+                        }
                     }
                 }
                 break;
@@ -164,10 +209,70 @@ int main(int argc, char *argv[])
     FMOD_System_Close(system);
     FMOD_System_Release(system);
 
-    SDL_FreeSurface(imageDeFond); /* On libère la surface */
+    SDL_FreeSurface(imageDeFond2); /* On libère la surface */
+    SDL_FreeSurface(fleche);
 
     TTF_CloseFont(police);
     TTF_Quit();
     SDL_Quit();
     return 0;
+}
+
+void afficher_titre_menu(SDL_Surface *ecran)
+{
+    SDL_Surface *chargerPartie;
+    SDL_Surface *creerPartie;
+    SDL_Surface *jouerClassement;
+    SDL_Surface *trouverSolution;
+    SDL_Surface *quitterJeu;
+    SDL_Surface *titre;
+
+    SDL_Rect positionCreerPartie;
+    SDL_Rect positionChargerPartie;
+    SDL_Rect positionJouerClassement;
+    SDL_Rect positionTrouverSolution;
+    SDL_Rect positionQuitterJeu;
+    SDL_Rect positionTitre;
+
+    titre = IMG_Load("titre.png");
+    positionTitre.x = ecran->w / 2 - titre->w / 2;
+    positionTitre.y = 10;
+
+    SDL_BlitSurface(titre, NULL, ecran, &positionTitre);
+    creerPartie = IMG_Load("creerLaby.png");
+    positionCreerPartie.x = 150;
+    positionCreerPartie.y = 100;
+
+    SDL_BlitSurface(creerPartie, NULL, ecran, &positionCreerPartie);
+
+    chargerPartie = IMG_Load("chargerLaby.png");
+    positionChargerPartie.x = 150;
+    positionChargerPartie.y = 140;
+
+    SDL_BlitSurface(chargerPartie, NULL, ecran, &positionChargerPartie);
+
+    jouerClassement = IMG_Load("jouerClassement.png");
+    positionJouerClassement.x = 150;
+    positionJouerClassement.y = 180;
+
+    SDL_BlitSurface(jouerClassement, NULL, ecran, &positionJouerClassement);
+
+    trouverSolution = IMG_Load("afficherSolution.png");
+    positionTrouverSolution.x = 150;
+    positionTrouverSolution.y = 220;
+
+    SDL_BlitSurface(trouverSolution, NULL, ecran, &positionTrouverSolution);
+
+    quitterJeu = IMG_Load("quitterJeu.png");
+    positionQuitterJeu.x = 150;
+    positionQuitterJeu.y = 260;
+
+    SDL_BlitSurface(quitterJeu, NULL, ecran, &positionQuitterJeu);
+
+    SDL_FreeSurface(titre);
+    SDL_FreeSurface(creerPartie);
+    SDL_FreeSurface(chargerPartie);
+    SDL_FreeSurface(jouerClassement);
+    SDL_FreeSurface(trouverSolution);
+    SDL_FreeSurface(quitterJeu);
 }
